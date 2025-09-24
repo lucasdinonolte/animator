1c7f3d10e8e362da71f4a82d03a77814a08efccc
Lucas Nolte <lucas@designsystems.international>
2025-09-24 20:53:31 +0200

    feat: add lifecycle events to transition function

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ type TransitionOptions<T> = {
   easing: Easing;
   direction: Direction;
   iterationCount: number;
+  onStart: () => void;
+  onUpdate: (progress: number) => void;
+  onComplete: () => void;
 };
 
 const defaultOptions: TransitionOptions<number> = {
@@ -20,6 +23,9 @@ const defaultOptions: TransitionOptions<number> = {
   easing: 'linear',
   iterationCount: 1,
   direction: 'forward',
+  onStart: () => {},
+  onUpdate: () => {},
+  onComplete: () => {},
 };
 
 /**
@@ -49,14 +55,40 @@ export const transition = <T>(_options: Partial<TransitionOptions<T>> = {}) => {
   const easingFn = resolveEasing(options.easing);
   const directionFn = resolveDirection(options.direction);
   const iterationCount = validateIterationCount(options.iterationCount);
-  const { from, to, duration, delay } = options;
+  const { from, to, duration, delay, onStart, onComplete, onUpdate } = options;
+
+  // As transitions are potentially being used in animations of various
+  // framerates, we cannot guarantee "pure" lifecycle values for our t
+  // values (such as 0 for start and 1 for end).
+  // 
+  // We therefore keep track of which lifecycle events have been fired,
+  // to allow for a more heuristic approach when to fire them. For example
+  // the `onStart` event will be fired as soon as the currentTime is bigger
+  // than 0 and `onStart` has not been fired before.
+  const lifecycle = {
+    hasStarted: false,
+    hasCompleted: false,
+  }
 
   const tickFn = (tick: number) => {
     const currentTime = Math.max(0, tick - delay);
 
+    if (!lifecycle.hasStarted && currentTime > 0) {
+      lifecycle.hasStarted = true;
+      onStart();
+    }
+
     // Scaling by 1000 turns any input made in seconds into milliseconds
     // and makes sure we can safely divide numbers
     const t = directionFn(currentTime * 1000, duration * 1000, iterationCount);
+
+    onUpdate(Math.min(1, t));
+
+    if (!lifecycle.hasCompleted && (currentTime * 1000) >= duration * 1000 * iterationCount) {
+      lifecycle.hasCompleted = true;
+      onComplete();
+    }
+
     return interpolate({ from, to }, easingFn(t));
   };
 
@@ -69,7 +101,7 @@ export const transition = <T>(_options: Partial<TransitionOptions<T>> = {}) => {
 };
 
 /**
- * Factor function builidng a pre-loaded sequential transition that
+ * Factory function builidng a pre-loaded sequential transition that
  * can be ticked on every frame.
  */
 export const sequentialTransition = <T>(
@@ -117,3 +149,35 @@ export const sequentialTransition = <T>(
     return transitions[transitions.length - 1]!.to;
   };
 };
+
+type Scene = {
+  duration: number;
+  scene: ({ globalTick, tick, playhead }: { globalTick: number; tick: number; playhead: number }) => void;
+}
+
+/**
+ * Combines multiple scenes into a single function that determines which scene
+ * should be active based on the current tick value.
+ * 
+ * @param scenes - An array of Scene objects, each containing a duration and a scene function.
+ * @returns A function that takes a tick value and executes the appropriate scene
+ *          based on the elapsed time.
+ */
+export const scenes = (...scenes: Array<Scene>) => {
+  return (tick: number) => {
+    let elapsed = 0;
+    for (let i = 0; i < scenes.length; i++) {
+      const scene = scenes[i]!;
+
+      if (tick >= elapsed && tick < elapsed + scene.duration) {
+        const currentTick = tick - elapsed;
+        const playhead = currentTick / scene.duration;
+
+        scene.scene({ globalTick: tick, tick: currentTick, playhead });
+        return;
+      }
+
+      elapsed += scene.duration;
+    }
+  };
+}
